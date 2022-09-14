4c35d20088a0d726989d86e8eb6a69f90611406e
tranhung0609 <trvanhung.nhk.0609@gmail.com>
2022-09-14 11:27:43 +0700

    dont copy file

---
+++ src/copyfile/TestCopyFile.java
@@ -0,0 +1,8 @@
+package copyfile;
+
+public class TestCopyFile {
+    public static void main(String[] args) {
+        CopyFile copyFile = new CopyFile();
+        copyFile.copyFile("E:\\DUANIJ\\Daicent\\src\\copyfile\\copyfile1.txt", "E:\\DUANIJ\\Daicent\\src\\copyfile\\copyfile2.txt");
+    }
+}
